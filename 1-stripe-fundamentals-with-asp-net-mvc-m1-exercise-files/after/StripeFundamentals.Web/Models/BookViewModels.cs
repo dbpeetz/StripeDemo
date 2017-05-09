7d18b126e6bf8943cec9b930739c1473df77f1ce
Devan Peetz <d.peetz516@gmail.com>
2017-05-08 23:00:41 -0400

    Add project files.

---
+++ 1-stripe-fundamentals-with-asp-net-mvc-m1-exercise-files/after/StripeFundamentals.Web/Models/BookViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StripeFundamentals.Models.Book
+{
+    public class IndexViewModel
+    {
+        public string StripePublishableKey { get; set; }
+    }
+}
---
+++ 1-stripe-fundamentals-with-asp-net-mvc-m1-exercise-files/before/StripeFundamentals.Web/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StripeFundamentals.Controllers
+{
+    public class BookController : Controller
+    {
+        // embedded form
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        //custom form
+        public ActionResult Custom() 
+        {
+            return View();
+        }
+    }
+}
